57159b13b0e39f5ff7ac527b0c576049a98ebadb
AHeraldOfTheNewAge <55515171+AHeraldOfTheNewAge@users.noreply.github.com>
2025-02-18 00:41:02 +0200

    Progressive?
    Trying to make the MHKalculator into a progressive web app

---
+++ service-worker.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = "pwa-cache-v1";
+const urlsToCache = [
+	"/",
+	"/index.html",
+	"/style3.css",
+	"/script.js",
+	"/icons/favicon.ico",
+	"/icons/icon-192.png",
+	"/icons/icon-512.png",
+	"/icons/icon-192-maskable.png",
+	"/icons/icon-512-maskable.png"
+];
+
+self.addEventListener("install", (event) => {
+	event.waitUntil(
+		caches.open(CACHE_NAME).then((cache) => {
+			return cache.addAll(urlsToCache);
+		})
+	);
+});
+
+self.addEventListener("fetch", (event) => {
+	event.respondWith(
+		caches.match(event.request).then((response) => {
+			return response || fetch(event.request);
+		})
+	);
+});
+
+self.addEventListener("activate", (event) => {
+	event.waitUntil(
+		caches.keys().then((cacheNames) => {
+			return Promise.all(
+				cacheNames.map((cache) => {
+					if (cache !== CACHE_NAME) {
+						return caches.delete(cache);
+					}
+				})
+			);
+		})
+	);
+});
