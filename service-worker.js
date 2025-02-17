5df412dfc1037db8eabf5f620addebabbe211494
AHeraldOfTheNewAge <55515171+AHeraldOfTheNewAge@users.noreply.github.com>
2025-02-18 01:59:26 +0200

    a

--- service-worker.js
+++ service-worker.js
@@ -5,8 +5,8 @@ const urlsToCache = [
 	"style3.css",
 	"script.js",
 	"favicon.ico",
-	"Tone.js",
-	"icons/icon-192.png",
+	"/Tone.js",
+	"/MHKalculator/icons/icon-192.png",
 	"icons/icon-512.png",
 	"icons/icon-192-maskable.png",
 	"icons/icon-512-maskable.png"
